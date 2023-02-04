687f184be88966c0336feb5f44c2feb4d2675668
Your Name <you@example.com>
2023-02-04 20:46:21 +0300

    commit fix webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,11 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
+    minimize: true,
+    minimizer: [new OptimizeCSSAssetsPlugin({}), '...'],
   },
 });
